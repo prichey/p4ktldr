165eda2e090a4c6cac340f8478cefbd0a47539d5
Preston Richey <prestonleerichey@gmail.com>
2019-01-28 23:21:27 -0500

    Now with more cache!

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,27 +1,64 @@
 import fetch from 'node-fetch';
+import * as lf from 'localforage';
+
+const store = {
+  suggestions: lf.createInstance({
+    name: 'suggestions'
+  }),
+  albums: lf.createInstance({
+    name: 'albums'
+  }),
+  artists: lf.createInstance({
+    name: 'artists'
+  })
+};
 
 export const getSuggestionsWithVal = async val => {
   if (val.length === 0) return [];
 
-  return fetch(`https://pitchfork.com/api/v2/search/_ac/?query=${val}`)
+  const cacheHit = await store.suggestions.getItem(val);
+
+  if (cacheHit) return cacheHit;
+
+  const result = await fetch(
+    `https://pitchfork.com/api/v2/search/_ac/?query=${val}`
+  )
     .then(res => res.json())
     .then(json => {
       return json.artists;
     });
+
+  await store.suggestions.setItem(val, result);
+  return result;
 };
 
 export const getAlbumsByArtistId = async (id, count = 100, start = 0) => {
-  return fetch(
+  const hashKey = JSON.stringify(id, count, start);
+  const cacheHit = await store.albums.getItem(hashKey);
+
+  if (cacheHit) return cacheHit;
+
+  const result = await fetch(
     `https://pitchfork.com/api/v2/entities/artists/${id}/albumreviews/?size=${count}&start=${0}`
   )
     .then(res => res.json())
     .then(res => res.results.list);
+
+  await store.albums.setItem(hashKey, result);
+  return result;
 };
 
 export const getArtist = async artistName => {
+  const cacheHit = await store.artists.getItem(artistName);
+
+  if (cacheHit) return cacheHit;
+
   const suggestions = await getSuggestionsWithVal(artistName).then(
     allSuggestions => allSuggestions.filter(artist => artist.id)
   );
 
-  return suggestions.length ? suggestions[0] : null;
+  const result = suggestions.length ? suggestions[0] : null;
+
+  await store.artists.setItem(artistName, result);
+  return result;
 };
