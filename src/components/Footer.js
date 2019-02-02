7c015ed59fc71d196187efd8fd7dc24934ab2064
Preston Richey <prestonleerichey@gmail.com>
2019-02-02 13:18:26 -0500

    Adjust mobile footer margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const StyledFooter = styled.footer`
   width: 100%;
   text-align: right;
   padding-top: 5px;
-  margin-bottom: 20px;
+  margin-bottom: 40px;
   flex-grow: 0;
   border-top: 3px solid ${props => props.theme.color.dark};
 
